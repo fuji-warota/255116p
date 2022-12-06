9a969d3e982cef9f2f99fe8735d9aaade5f18821
Fuji_warota <255116p@gs.kochi-tech.ac.jp>
2022-12-06 15:18:14 +0900

    test1

--- main.go
+++ main.go
@@ -11,10 +11,17 @@ func main() {
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, ping())
 	})
+  e.GET("/pong", func(c echo.Context) error {
+		return c.String(http.StatusOK, pong())
+	})
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func ping() string {
 	return "pong"
+}
+
+func pong() string {
+	return "ping"
 }
